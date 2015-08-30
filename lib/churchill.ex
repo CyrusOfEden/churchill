62071f5a978a6c38c6162ebcbd88a9886e9149bf
Kash Nouroozi <knouroozi@gmail.com>
2015-08-30 17:55:38 -0400

    Tests + Inline docs

--- lib/churchill.ex
+++ lib/churchill.ex
@@ -1,19 +1,31 @@
 defmodule Churchill do
+  @doc """
+  Parallel map a collection.
+
+  `collection`: Anything that implements the Enumerable protocol
+  `fun`: The mapping function
+  `link`: Whether to link the parallel worker to the current process. Defaults to `true`.
+  `timeout`: The amount of time to wait for the worker to finish. Defaults to `5000`.
+
+  ## Examples
+
+      iex> Churchill.pmap(1..10, &(&1 + 5))
+      [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  """
   def pmap(collection, fun, link \\ true, timeout \\ 5000) when is_function(fun) do
     master = self()
-    worker = task(link)
-    process = &send(master, {self(), fun.(&1)})
 
-    processor = fn item ->
-      worker.(fn -> process.(item) end)
+    process = &send(master, {self(), fun.(&1)})
+    worker = case link do
+      true ->
+        &Task.start_link(fn -> process.(&1) end)
+      false ->
+        &Task.start(fn -> process.(&1) end)
     end
     receiver = fn {:ok, pid} ->
       receive do: ({^pid, result} -> result), after: (timeout -> nil)
     end
 
-    collection |> Enum.map(processor) |> Enum.map(receiver)
+    collection |> Enum.map(worker) |> Enum.map(receiver)
   end
-
-  defp task(true),  do: &Task.start_link(&1)
-  defp task(false), do: &Task.start(&1)
 end
---
+++ test/churchill_test.exs
@@ -0,0 +1,10 @@
+defmodule ChurchillTest do
+  use ExUnit.Case
+
+  @a Enum.to_list(1..1000)
+  @b Enum.shuffle(@a)
+
+  test "parallel map" do
+    assert Churchill.pmap(@a, &(&1 * &1)) == Enum.map(@a, &(&1 * &1))
+  end
+end
